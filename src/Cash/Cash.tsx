f22a3091e2005958753f8b22e74af9f511537ab6
popetrov <popetrov64@gmail.com>
2021-12-02 23:21:34 +0400

    basic logical cash-mashine

--- src/Cash/Cash.tsx
+++ src/Cash/Cash.tsx
@@ -1,43 +1,75 @@
 import React, { useState } from "react"
+import { GettingCash } from "./TextTerminal"
 
 export const Cash = ()=>{
     
+    const inputRef = React.createRef<HTMLInputElement>()
     const [inputText, setInputText] = useState("")
-
+    const[result5000, setResult5000] = useState(0)
+    const[result2000, setResult2000] = useState(0)
+    const [result1000, setResult1000] = useState(0)
+    const[result500, setResult500] = useState(0)
+    const[result100, setResult100] = useState(0)
+    const[result50, setResult50] = useState(0)
+    const[remainBalance, setRemainBalance] = useState(0)
+    
     const getCash = () =>{
-        const arrayDenominations = [1000, 500, 100, 50]
+        setResult5000(0)
+        setResult2000(0)
+        setResult1000(0)
+        setResult500(0)
+        setResult100(0)
+        setResult50(0)
+
+        const arrayDenominations = [5000, 2000, 1000, 500, 100, 50]
         const resultCash = []
         let amountRequest = Number(inputRef.current!.value)
-        if(Number(inputRef.current!.value)<0){
-            console.log('введите необходимую сумму')
-        }
         for(let i=0; i<arrayDenominations.length;i++){
             let note = arrayDenominations[i]
-
-            while(Number(inputRef.current!.value)-note>=0){
+            while(amountRequest-note>=0){
                 amountRequest-=note
-                resultCash.push(inputRef.current!.value)
+                resultCash.push(note)
             }
         }
-        return console.log(resultCash)
+        for(let j of resultCash){
+           switch(j){
+               case 5000:
+                setResult5000(prev=>(prev+=1))
+                break;
+               case 2000:
+                setResult2000(prev=>(prev+=1))
+                break;
+               case 1000:
+                setResult1000(prev=>(prev+=1))
+                break;
+               case 500:
+                setResult500(prev=>(prev+=1))
+                break;
+               case 100:
+                setResult100(prev=>(prev+=1))
+                break;
+               case 50:
+                setResult50(prev=>(prev+=1))
+                break     
+           }
+        }
+        console.log()
+        return (setRemainBalance(amountRequest))
     }
+
+
 
     const handleChange = ()=>{
         setInputText(inputRef.current!.value)
     }
 
-    const inputRef = React.createRef<HTMLInputElement>()
 
     return(
         <div>
             <input ref={inputRef} type="text" placeholder="введите требуемую сумму" value={inputText} onChange={handleChange}/>
             <div>
                 <button onClick={getCash}>Получить деньги</button>
-                <h1>Итого к выдаче</h1>
-                <p>купюр номиналом 1000: </p>
-                <p>купюр номиналом 500: </p>
-                <p>купюр номиналом 100: </p>
-                <p>купюр номиналом 50: </p>
+                <GettingCash note1000={result1000} note500={result500} note100={result100} note50={result50} remainBalance={remainBalance} note5000={result5000} note2000={result2000}/>
             </div>
         </div>
     )
